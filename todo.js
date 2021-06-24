79ea0d0fb7b071bd8525113725a362150bc9e821
shubham8528 <shubham.bisht@etelligens.in>
2021-06-24 18:18:23 +0530

    todo App

--- todo.js
+++ todo.js
@@ -1,3 +1,17 @@
+var addinputtext= document.getElementById('inputtext');
+var addbtn= document.getElementById('btn');
+var addcontainer =document.getElementById('todocontainer');
 
+addbtn.addEventListener('click', function(){
+	var text = document.createElement('p');
+		text.innerText = addinputtext.value;
+		addcontainer.appendChild(text);
+});
 
-document.getElementById('inputtext').style.background='red';
+function abc(){
+	document.getElementById('inputtext').style.background='orange';
+
+}
+
+ 
+ 

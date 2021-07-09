9d4d43e7c849898b040946c360910f859a8c07de
shubham8528 <shubham.bisht@etelligens.in>
2021-07-09 16:30:59 +0530

    Todo App

--- todo.js
+++ todo.js
@@ -1,18 +1,7 @@
 var addinput = document.querySelector("#inputtext");
 var addbtn = document.querySelector("#btn");
-var addtodocontainer = document.querySelector("#todoconatoiner");
-
-var todoList = [
-  {
-    id: 1,
-    title: "1 Kg Tomato",
-  },
-  {
-    id: 1,
-    title: "5 Kg Patato",
-  },
-];
-
+var addListe = document.querySelector(".todoList");
+var idCount= 0
 // Event Call
 addbtn.addEventListener("click", addTodo);
 
@@ -39,16 +28,17 @@ function addTodo(even) {
 
   // Delete Button
   var todoBtn2 = document.createElement("button");
+  todoBtn2.setAttribute("onclick", "abc(event);");
   todoBtn2.innerHTML = '<i class="fa fa-trash-o" style="font-size:20px"></i>';
   todoBtn2.classList.add("deletBtn");
   todoBtn2.style = "background-Color:gray; border:none";
   todoDiv.appendChild(todoBtn2);
-
+  todoDiv.setAttribute('id',idCount)
   // Show TODO list append
-  todocontainer.appendChild(todoDiv);
+  addListe.appendChild(todoDiv);
 
   ////////////  POST TODO DATA///////////////////////////
-
+ console.log(idCount)
   fetch("https://60dadebc801dcb0017290c2f.mockapi.io/todos", {
     method: "post",
     headers: {
@@ -57,10 +47,14 @@ function addTodo(even) {
     body: JSON.stringify({ title: newTodo.innerText }),
   })
     .then(function (insurt) {
-      reture(insurt);
+      return insurt;
     })
     .then(function (result) {
-      document.write(result);
+      idCount++
+      // document.write(result);
+      var x = document.getElementById("toast")
+      x.className = "show";
+      setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
     });
 }
 
@@ -77,16 +71,26 @@ function render() {
       return insurt.json();
     })
     .then(function (result) {
+     
+      console.log(idCount)
       renderTodos(result);
+    })
+    .catch(function (error) {
+      document.write( error,"Something Error Here");
     });
 }
 
 function renderTodos(todos) {
   // console.log(result);
-  todocontainer.innerHTML = "";
+
   todos.forEach((e) => {
     var todoDiv = document.createElement("div");
     todoDiv.classList.add("todo-here");
+   todoDiv.setAttribute("id",e.id)
+   idCount = e.id
+
+    // Show TODO list append
+    addListe.appendChild(todoDiv);
 
     // list Create
     var newTodo = document.createElement("li");
@@ -104,13 +108,11 @@ function renderTodos(todos) {
 
     // Delete Button
     var todoBtn2 = document.createElement("button");
+    todoBtn2.setAttribute("onclick", "abc(event);");
     todoBtn2.innerHTML = '<i class="fa fa-trash-o" style="font-size:20px"></i>';
     todoBtn2.classList.add("deletBtn");
     todoBtn2.style = "background-Color:gray; border:none";
     todoDiv.appendChild(todoBtn2);
-
-    // Show TODO list append
-    todocontainer.appendChild(todoDiv);
   });
 }
 
@@ -118,3 +120,25 @@ window.onload = function () {
   render();
   // renderTodos(todoList);
 };
+function abc(event){
+console.log(event.path[2].id)
+var id = Number(event.path[2].id) 
+document.getElementById(event.path[2].id).outerHTML=""
+
+fetch(`https://60dadebc801dcb0017290c2f.mockapi.io/todos/${id}`, {
+  method: "delete",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then(function (insurt) {
+    return insurt;
+  })
+  .then(function (result) {
+    idCount++
+    // document.write(result);
+    var x = document.getElementById("toastdelete")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+  });
+}
